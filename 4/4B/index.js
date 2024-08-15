1fd6e5bb7770e1b06b1260f4994eee5ba826deab
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-16 03:32:56 +0700

    feat: CRUD,AUTH

--- 4/4B/index.js
+++ 4/4B/index.js
@@ -1,47 +1,194 @@
 // import module
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 const port = 3000;
 
 const path = require("path");
-const hbs = require("hbs");
-
+const UserSession = require("express-session");
 const { Sequelize, QueryTypes } = require("sequelize");
 const config = require("./config/config.json");
 const sequelize = new Sequelize(config.development);
+const multer = require("multer");
+const { use } = require("bcrypt/promises");
+const upload = multer({ dest: "./src/uploads" });
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "hbs");
-app.set("views","src/views")
+app.set("views", "src/views");
 
 app.use("/assets", express.static(path.join(__dirname, "src/assets")));
 app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
 
-app.get("/", home);
-app.get("/login", login)
-app.get("/register", register)
-app.get("/detail_hero", detailHero_page)
-app.get("/update_hero", updateHero_page)
-app.get("/add_hero", addHero_page)
+app.use(
+  UserSession({
+    name: "hero-session",
+    secret: "SupraBapak",
+    resave: false,
+    saveUninitialized: true,
 
-function home(req, res){
-  res.render("index")
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+app.get("/", home_render);
+app.get("/login", login_render);
+app.get("/register", register_render);
+app.get("/logout", logout);
+app.post("/login", loginbyEmail);
+app.post("/register", registerbyUser);
+
+app.get("/detail_hero", detailHero_render);
+app.get("/detail_hero/:id", detailHerobyId);
+
+app.get("/update_hero", updateHero_render);
+app.get("/update_hero", updateHerobyId);
+
+app.get("/delete_hero", updateHerobyId);
+app.get("/edit_hero", updateHerobyId);
+
+app.get("/add_hero", addHero_render);
+app.post("/add_hero", upload.single("photo"), addHerobyId);
+
+async function home_render(req, res) {
+  const { isLogin } = req.session;
+
+  const query = "SELECT * FROM heroes_tbs ORDER BY id ASC ";
+  const result = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  res.render("index", {
+    data: result,
+    isLogin,
+  });
 }
-function login(req, res){
-  res.render("login")
+function login_render(req, res) {
+  res.render("login");
 }
-function register(req, res){
-  res.render("register")
+function register_render(req, res) {
+  res.render("register");
 }
-function detailHero_page(req, res){
-  res.render("detail_hero")
+function logout(req, res) {
+  req.session.destroy(function (err) {
+    if (err) return console.error("Logout failed!");
+
+    console.log("Logout success!");
+    res.redirect("/");
+  });
 }
-function updateHero_page(req, res){
-  res.render("update_hero")
+async function loginbyEmail(req, res) {
+  const { email, password } = req.body;
+
+  const query = `SELECT * FROM users_tbs WHERE email = '${email}'`;
+  const userlogin = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  if (!userlogin.length) {
+    console.log("Email tidak ditemukan");
+    return res.redirect("/login");
+  }
+  bcrypt.compare(password, userlogin[0].password, function (err, result) {
+    if (!result) {
+      console.log("Password tidak sesuai!");
+      return res.redirect("/login");
+    } else {
+      req.session.isLogin = true;
+      req.session.user = userlogin[0].name;
+      req.session.idUser = userlogin[0].id;
+
+      console.log("Login Berhasil!");
+      return res.redirect("/");
+    }
+  });
 }
-function addHero_page(req, res){
-  res.render("add_hero")
+
+async function registerbyUser(req, res) {
+  try {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = `INSERT INTO users_tbs( 
+    email, 
+    username, 
+    password ,
+    "createdAt", 
+    "updatedAt"
+    ) 
+    VALUES (
+    '${email}',
+    '${username}',
+    '${hashedPassword}',
+     NOW(),
+     NOW())`;
+
+    await sequelize.query(query, { type: QueryTypes.INSERT });
+
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
+}
+function detailHero_render(req, res) {
+  const { isLogin, user } = req.session;
+
+  isLogin
+    ? res.render("detail_hero", {
+        isLogin,
+        user,
+      })
+    : res.redirect("/login");
+}
+async function detailHerobyId(req, res) {
+  const { id } = req.params;
+  const { isLogin, user } = req.session;
+
+  const query = `SELECT * FROM heroes_tbs WHERE id=${id}`;
+  const result = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  res.render("detail_hero", {
+    data: result[0],
+    isLogin,
+    user,
+  });
+}
+function updateHero_render(req, res) {
+  isLogin ? res.render("update_hero") : res.redirect("/login");
+}
+function updateHerobyId(req, res) {
+  res.render("update_hero");
+}
+async function addHero_render(req, res) {
+  const { isLogin, user } = req.session;
+  const query = `SELECT * FROM type_tbs`;
+  const typehero = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  isLogin
+    ? res.render("add_hero", { data: typehero, isLogin, user })
+    : res.redirect("/login");
+}
+async function addHerobyId(req, res) {
+  const { name, typehero } = req.body;
+
+  const photo = req.file ? req.file.filename : null;
+  const id = req.session.idUser;
+  const query = `INSERT INTO heroes_tbs (
+  name, 
+  type_id, 
+  photo, 
+  user_id,
+  "createdAt", 
+  "updatedAt"
+  )
+  VALUES ('${name}','${typehero}','${photo}','${id}',NOW(),NOW())`;
+
+  const result = await sequelize.query(query, { type: QueryTypes.INSERT });
+  console.log(result);
+  res.render("add_hero");
 }
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
