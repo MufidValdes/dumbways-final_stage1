2c9088320e6d5e2a2de97e078e6252770159c33f
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-15 13:21:24 +0700

    feat: create db with sequelize

---
+++ 4/4A/migrations/20240815061426-create-heroes-tb.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('heroes_tbs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model : "type_tbs",
+          key: "id"
+        }
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model : "users_tbs",
+          key: "id"
+        }  
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('heroes_tbs');
+  }
+};
