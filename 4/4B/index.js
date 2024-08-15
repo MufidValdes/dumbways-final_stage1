1dc3b5cadaa0b9fec24902ba54d074cf0e6746d5
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-16 00:00:03 +0700

    feat: route to hbs

--- 4/4B/index.js
+++ 4/4B/index.js
@@ -1,14 +1,46 @@
-// import module 
-const express = require('express')
-const { Sequelize } = require('sequelize');
-const config = require('./config/config.json')
-const sequelize = new Sequelize(config.development)
-const app = express()
-const port = 3000
+// import module
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const path = require("path");
+const hbs = require("hbs");
+
+const { Sequelize, QueryTypes } = require("sequelize");
+const config = require("./config/config.json");
+const sequelize = new Sequelize(config.development);
+
+app.set("view engine", "hbs");
+app.set("views","src/views")
+
+app.use("/assets", express.static(path.join(__dirname, "src/assets")));
+app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
+
+app.get("/", home);
+app.get("/login", login)
+app.get("/register", register)
+app.get("/detail_hero", detailHero_page)
+app.get("/update_hero", updateHero_page)
+app.get("/add_hero", addHero_page)
+
+function home(req, res){
+  res.render("index")
+}
+function login(req, res){
+  res.render("login")
+}
+function register(req, res){
+  res.render("register")
+}
+function detailHero_page(req, res){
+  res.render("detail_hero")
+}
+function updateHero_page(req, res){
+  res.render("update_hero")
+}
+function addHero_page(req, res){
+  res.render("add_hero")
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
