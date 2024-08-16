ecf69c9c0983e5e289188454e1c2ef477ab7f5e8
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-16 19:01:58 +0700

    feat: change to  prisma

--- 1/1.js
+++ 1/1.js
@@ -1,14 +1,31 @@
 function drawSikuSiku(n) { //n = panjang
-    let angka = 1; // Mulai dengan angka 1
+    let angka = 2; // Mulai dengan angka 2
     let baris = ''; // Variabel untuk menyimpan baris saat ini
     // Loop untuk setiap baris dari segitiga
     for (let i = 0; i < n; i++) {
         for (let j = 0; j <= i; j++) {
             baris += angka + ' '; // Menambahkan angka ke baris
-            angka++; // Bergerak ke angka berikutnya
+            angka= nextPrime(angka + 1); // Bergerak ke angka berikutnya
         }
         baris += '\n';
     }
     return baris;
+}
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+function nextPrime(start) {
+    let num = start;
+    while (!isPrime(num)) {
+        num++;
+    }
+    return num;
 }
 console.log(drawSikuSiku(7));
