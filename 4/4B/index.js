ee4cb9e7b14a3d523026ef09c43e5a2d7d23faa6
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-08-16 13:33:40 +0700

    feat: update

--- 4/4B/index.js
+++ 4/4B/index.js
@@ -10,7 +10,7 @@ const { Sequelize, QueryTypes } = require("sequelize");
 const config = require("./config/config.json");
 const sequelize = new Sequelize(config.development);
 const multer = require("multer");
-const { use } = require("bcrypt/promises");
+
 const upload = multer({ dest: "./src/uploads" });
 app.use(express.urlencoded({ extended: true }));
 
@@ -36,53 +36,51 @@ app.use(
 );
 
 app.get("/login", login_render);
-app.post("/login", loginbyEmail);
-
 app.get("/register", register_render);
-app.post("/register", registerbyUser);
-
 app.get("/logout", logout);
-
 app.get("/", home_render);
 app.get("/add_hero", addHero_render);
-app.post("/add_hero", upload.single("photo"), addHerobyId);
-
 app.get("/detail_hero", detailHero_render);
 app.get("/detail_hero/:id", detailHerobyId);
-
 app.get("/delete_hero/:id", deleteHerobyId);
+app.get("/add_type", addtype_render);
+app.get("/delete_type/:typeid", deletetype_byid);
+app.get("/edit_type/:typeid", edittype_render);
+
+app.post("/login", loginbyEmail);
+app.post("/register", registerbyUser);
+app.post("/add_hero", upload.single("photo"), addHerobyId);
+app.post("/add_type", addtype_byId);
+app.post("/edit_type/:typeid", edittype_byId);
 
 app.get("/edit_hero/:id", editHero_render);
 app.post("/edit_hero/:id",upload.single("photo"), editHerobyId);
 
-app.get("/add_type", addtype_render);
-app.post("/add_type", addtype_byId);
 
-app.get("/delete_type/:id", deletetype_byid);
-app.get("/edit_type/:id", edittype_render);
-app.post("/edit_type/:id", edittype_byId);
 
 
 
 async function deletetype_byid(req, res) {
-  const {id} = req.params;
+  const id = req.params.typeid;
 
   const query = `DELETE FROM type_tbs WHERE id='${id}'`;
   await sequelize.query(query, { type: QueryTypes.DELETE });
 
   res.redirect('/add_type'); 
 }
-
+function type_render(req, res) {
+  res.render('/edit_type') 
+}
 async function edittype_render(req, res) {
-  const  {id } = req.params
+  const id = req.params.typeid;
   
   const query = `SELECT * FROM type_tbs WHERE id='${id}'`;
-  await sequelize.query(query, { type: QueryTypes.UPDATE });
+  const typeheroes =await sequelize.query(query, { type: QueryTypes.UPDATE });
 
-  res.render('/edit_type'); 
+  res.render('edit_type', {data: typeheroes[0]}); 
 }
 async function edittype_byId(req, res) {
-  const  {id } = req.params
+  const id = req.params.typeid;
   const { nametype } = req.body;
 
   const query = `UPDATE type_tbs SET name = '${nametype}' WHERE id='${id}' `;
@@ -247,8 +245,8 @@ async function editHerobyId(req, res) {
 
 async function editHero_render(req, res) {
   const { isLogin, user} = req.session;
-
-  const typequery = `SELECT * FROM type_tbs`;
+  const {id} = req.params;
+  const typequery = `SELECT * FROM heroes_tbs WHERE id='${id}'`;
   const typehero = await sequelize.query(typequery, { type: QueryTypes.SELECT });
 
   isLogin ? 
